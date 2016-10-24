cd05183fb39dec43b326b00795eb6b5f28979c50
Ivan Petrov <ivanpetrov0005@gmail.com>
2016-10-24 20:21:12 +0300

    add project

---
+++ src/main/java/com/peim/DemoDjtApplication.java
@@ -0,0 +1,12 @@
+package com.peim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoDjtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoDjtApplication.class, args);
+	}
+}
