ba3a4c48a90d1e9cde91df81b8e2bd1b863576b8
Ivan Petrov <ivanpetrov0005@gmail.com>
2016-10-27 20:26:20 +0300

    refactoring

--- src/main/java/com/peim/controller/AsyncController.java
+++ src/main/java/com/peim/controller/AsyncController.java
@@ -1,6 +1,5 @@
 package com.peim.controller;
 
-import com.peim.service.dao.TaskService;
 import com.peim.model.Task;
 import com.peim.service.TaskProcessingService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,14 +10,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.async.DeferredResult;
 
-import java.util.concurrent.CompletableFuture;
-
 @RestController
 @RequestMapping(path = "/api")
 public class AsyncController {
-
-    @Autowired
-    private TaskService taskService;
 
     @Autowired
     private TaskProcessingService taskProcessingService;
@@ -26,32 +20,14 @@ public class AsyncController {
     @RequestMapping(path = "/task/execute/{id}", method = RequestMethod.GET)
     public DeferredResult<ResponseEntity<Task>> execute(@PathVariable(value = "id") int id) {
         final DeferredResult<ResponseEntity<Task>> deferredResult = new DeferredResult<>(300000L);
-        Task task = taskService.getTaskById(id);
-        CompletableFuture<String> future = taskProcessingService.process(task);
-        future.whenComplete((success, failure) -> {
-            try {
-                if (failure != null) {
-                    task.setStatus("FAILURE");
-                    task.setDescription(failure.getMessage());
-                } else {
-                    task.setStatus("SUCCESS");
-                    task.setDescription(success);
-                }
-                taskService.updateTask(task);
-                deferredResult.setResult(ResponseEntity.ok(task));
-            } catch (Exception e) {
-                deferredResult.setErrorResult(ResponseEntity.badRequest());
-                e.printStackTrace();
-            }
-        });
+        taskProcessingService.execute(id, deferredResult);
         return deferredResult;
     }
 
     @RequestMapping(path = "/task/cancel/{id}", method = RequestMethod.PUT)
     public DeferredResult<ResponseEntity<Task>> cancel(@PathVariable(value = "id") int id) {
         final DeferredResult<ResponseEntity<Task>> deferredResult = new DeferredResult<>(300000L);
-        Task task = taskService.getTaskById(id);
-        deferredResult.setResult(ResponseEntity.ok(task));
+        taskProcessingService.cancel(id, deferredResult);
         return deferredResult;
     }
 }
--- src/main/java/com/peim/service/TaskProcessingService.java
+++ src/main/java/com/peim/service/TaskProcessingService.java
@@ -1,19 +1,48 @@
 package com.peim.service;
 
+import com.peim.service.dao.TaskService;
 import com.peim.service.hash.HashAlgorithm;
 import com.peim.service.hash.HashAlgorithmFactory;
 import com.peim.model.Task;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.context.request.async.DeferredResult;
 
 import java.util.concurrent.CompletableFuture;
 
 @Service("taskProcessingService")
 public class TaskProcessingService {
 
-    public CompletableFuture<String> process(Task task) {
+    @Autowired
+    private TaskService taskService;
+
+    public void execute(int id, DeferredResult<ResponseEntity<Task>> deferredResult) {
+        Task task = taskService.getTaskById(id);
         HashAlgorithm algorithm = HashAlgorithmFactory.of(task.getAlgo());
-        return CompletableFuture.supplyAsync(
+        CompletableFuture<String> future = CompletableFuture.supplyAsync(
                 () -> algorithm.hash(task.getSrc())
         );
+        future.whenComplete((success, failure) -> {
+            try {
+                if (failure != null) {
+                    task.setStatus("FAILURE");
+                    task.setDescription(failure.getMessage());
+                } else {
+                    task.setStatus("SUCCESS");
+                    task.setDescription(success);
+                }
+                taskService.updateTask(task);
+                deferredResult.setResult(ResponseEntity.ok(task));
+            } catch (Exception e) {
+                deferredResult.setErrorResult(ResponseEntity.badRequest());
+                e.printStackTrace();
+            }
+        });
+    }
+
+    public void cancel(int id, DeferredResult<ResponseEntity<Task>> deferredResult) {
+        Task task = taskService.getTaskById(id);
+        //............
     }
 }
